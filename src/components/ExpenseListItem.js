fcf24d947198746162ef92de0f0734cbc31976fd
201519040032 <dimiortt@gmail.com>
2020-09-29 07:31:47 -0400

    setup createdAt format

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
+import moment from 'moment'
 
 const ExpenseListItem = ({ description, amount, createdAt, id }) => (
   <div>
     <NavLink to={'edit/' + id}>
       <h3>{description}</h3>
     </NavLink>
-    <p>{amount} - {createdAt}</p>
+    <p>
+      {amount}{' - '}
+      {moment(createdAt).format('MMMM Do, YYYY')}
+    </p>
   </div>
 )
 
